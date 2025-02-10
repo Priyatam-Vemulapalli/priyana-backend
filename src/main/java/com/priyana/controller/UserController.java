0a8a8b2b3e515e54e9b93e148c5977d1e4a40e2a
Priyatam-Vemulapalli <priyatamvemulapalli@gmail.com>
2025-02-10 18:31:43 +0530

    firebase setup

--- src/main/java/com/priyana/controller/UserController.java
+++ src/main/java/com/priyana/controller/UserController.java
@@ -3,11 +3,14 @@ package com.priyana.controller;
 import com.priyana.DTO.RequestBody.UserLoginBody;
 import com.priyana.DTO.RequestBody.UserRequestBody;
 import com.priyana.DTO.ResponseBody.UserResponseBody;
+import com.priyana.model.User;
 import com.priyana.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/auth/user")
@@ -17,17 +20,16 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/register")
-    private ResponseEntity<UserResponseBody> RegisterUser(@RequestBody UserRequestBody userRequestBody){
-
-
-        return new ResponseEntity<>(userService.RegisterUser(userRequestBody), HttpStatus.OK);
-
+    private ResponseEntity<UserResponseBody> RegisterUser(@RequestBody User user){
+        UserResponseBody userResponseBody = userService.RegisterUser(user);
+        return new ResponseEntity<>(userResponseBody, HttpStatus.CREATED);
     }
 
-    @PostMapping("/login")
-    private ResponseEntity<UserResponseBody> LoginUser(@RequestBody UserLoginBody userLoginBody){
+    @GetMapping("/{id}")
+    private ResponseEntity<UserResponseBody> getUser(@PathVariable UUID userid){
 
-        return new ResponseEntity<>(userService.LoginUser(userLoginBody), HttpStatus.FOUND);
+        UserResponseBody userResponseBody = userService.findUserById(userid);
+        return new ResponseEntity<>(userResponseBody,HttpStatus.OK);
     }
 
 }
